c923d5c48641d3a482e0605efc433363ef952c6e
LeonardoConde1 <conde.dd0@gmail.com>
2023-11-27 23:51:43 -0800

    correcion de import navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Header from "./components/Header.vue";
-import Navbar from "./components/navbar.vue";
+import Navbar from "./components/Navbar.vue";
 import Content from "./components/Content.vue";
 import List from "./components/list.vue";
 import Cards from "./components/Cards.vue";
